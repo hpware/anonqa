44a22beb46c4741e2fc69f5f7426cdb9be40dcde
吳元皓 <hw@yuanhau.com>
2025-07-31 19:09:35 +0800

    Add convex and page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <div className="font-sans">
-      <div>vvv</div>
+      <div className="absolute inset-0 flex flex-col justify-center text-center">
+        <h2 className="bg-clip-text bg-gradient-to-br from-purple-600 to-blue-600 text-6xl text-bold text-transparent">
+          An app
+        </h2>
+      </div>
     </div>
   );
 }
