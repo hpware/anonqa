24bfb4843ad1614536e1804b25cc89bece3b2636
hpware <hw@yuanhau.com>
2025-09-18 20:36:45 -0500

    Update route.ts

--- web/app/api/social/threads/publish/route.ts
+++ web/app/api/social/threads/publish/route.ts
@@ -1,7 +1,7 @@
-import { api } from "@/convex/_generared/api"
-import { fetchQuery, fetchMutation } from "convex/nextjs"
-import { NextRequest } from "next/server"
-import { cookies } from "next/headers"
+import { api } from "@/convex/_generated/api";
+import { fetchQuery, fetchMutation } from "convex/nextjs";
+import { NextRequest } from "next/server";
+import { cookies } from "next/headers";
 
 export const GET = async (request: NextRequest) => {
 	const cookie = await cookies();
@@ -9,7 +9,8 @@ export const GET = async (request: NextRequest) => {
 	try {
 	const checkSession = await fetchQuery(api.func_users.verifySession, { currentSession: session });
 	} catch (e) {
-
+console.log(e)
+return new Response("server side error");
 	}
   //const req = await fetch(
   //  `https://graph.threads.net/v1.0/${post_id}/threads_publish?domain=THREADS&creation_id=17924832948103188&access_token=${access_token}`,
