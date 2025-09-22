1851ab695361245e6994cffb54c12191c53f4c78
吳元皓 <hw@yuanhau.com>
2025-09-22 14:34:37 +0800

    yeah can I logout now?

--- web/app/auth/logout/route.ts
+++ web/app/auth/logout/route.ts
@@ -3,5 +3,5 @@ import { cookies } from "next/headers";
 export const GET = async () => {
   const cookie = await cookies();
   cookie.delete("session");
-  return Response.redirect("/auth/login");
+  return Response.redirect("/auth/login", 302);
 };
